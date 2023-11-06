e04e5967abc10fa801f7441a2e2b8c5dc21e6b70
waryad <waryad@sheridancollege.ca>
2023-11-05 22:01:34 -0500

    Add project files.

---
+++ Final_DeepinderKaurWarya/AddNewProductWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Final_DeepinderKaurWarya
+{
+    public partial class AddNewProductWindow : Window
+    {
+        public AddNewProductWindow()
+        {
+            InitializeComponent();
+            LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                var categories = context.Categories.ToList();
+                CmbNewProductCategory.ItemsSource = categories;
+            }
+        }
+
+
+        private void BtnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            // Check if product name or category is not entered
+            if (string.IsNullOrWhiteSpace(TxtNewProductName.Text) || CmbNewProductCategory.SelectedValue == null)
+            {
+                MessageBox.Show("Please enter the required information (Product Name and Category) before adding a product.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                return;
+            }
+
+            // Check if price is empty or cannot be parsed as a decimal
+            if (string.IsNullOrWhiteSpace(TxtPrice.Text) || !decimal.TryParse(TxtPrice.Text, out decimal parsedPrice))
+            {
+                MessageBox.Show("Please enter a valid price before adding a product.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            using (var context = new NorthwindEntities())
+            {
+                Product newProduct = new Product
+                {
+                    ProductName = TxtNewProductName.Text,
+                    UnitPrice = parsedPrice,  // Use the parsed price here
+                    CategoryID = (int)CmbNewProductCategory.SelectedValue
+                };
+
+                context.Products.Add(newProduct);
+                context.SaveChanges();
+            }
+
+            MessageBox.Show("Product added successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Final_DeepinderKaurWarya/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Final_DeepinderKaurWarya
+{
+    public partial class MainWindow : Window
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> DisplayedProducts { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadCategories();
+            
+        }
+
+        private void LoadCategories()
+        {
+
+            using (var context = new NorthwindEntities())
+            {
+                Categories = context.Categories.ToList();
+                CmbCategories.ItemsSource = Categories; // Add this line
+                CmbCategories.DisplayMemberPath = "CategoryName";
+                CmbCategories.SelectedValuePath = "CategoryID";
+            }
+        }
+        private void LoadProducts()
+        {
+            using (var context = new NorthwindEntities())
+            {
+                DisplayedProducts = context.Products.ToList();
+                dgProducts.ItemsSource = DisplayedProducts;
+            }
+        }
+
+        private void LoadProductsByCategory(int categoryId)
+        {
+            using (var context = new NorthwindEntities())
+            {
+                DisplayedProducts = context.Products
+                    .Where(p => p.CategoryID == categoryId)
+                    .ToList();
+                dgProducts.ItemsSource = DisplayedProducts;
+            }
+        }
+
+        private void BtnGetAllProducts_Click(object sender, RoutedEventArgs e)
+        {
+            LoadProducts();
+        }
+
+        private void BtnClearData_Click(object sender, RoutedEventArgs e)
+        {
+            CmbCategories.SelectedIndex = -1;
+            TxtProductName.Clear();
+            dgProducts.ItemsSource = null;
+            DisplayedProducts?.Clear();  // Add this line
+        }
+
+        private void BtnSearchProduct_Click(object sender, RoutedEventArgs e)
+        {
+            using (var context = new NorthwindEntities())
+            {
+                string productName = TxtProductName.Text.Trim().ToLower();
+                var products = context.Products
+                    .Where(p => p.ProductName.ToLower().Contains(productName))
+                    .ToList();
+                dgProducts.ItemsSource = products;
+            }
+        }
+
+        private void CmbCategories_SelectionChanged(object sender, SelectionChangedEventArgs e) // Add this method
+        {
+            if (CmbCategories.SelectedItem is Category selectedCategory)
+            {
+                LoadProductsByCategory(selectedCategory.CategoryID);
+            }
+        }
+
+        private void BtnAddNewProduct_Click(object sender, RoutedEventArgs e)
+        {
+            AddNewProductWindow addNewProductWindow = new AddNewProductWindow();
+            addNewProductWindow.ShowDialog();
+            // Reload products after the Add New Product window is closed
+            LoadProducts();
+        }
+    }
+}
